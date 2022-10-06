36cc11202dcbd2f64b06ac6d03485e4c2b4274ad
Kwasi Antwi Baayeh <kwasiantwibaayeh@gmail.com>
2022-10-06 09:34:41 +0000

    Add the test for updating status

--- src/modules/__test__/localStorage.test.js
+++ src/modules/__test__/localStorage.test.js
@@ -73,6 +73,54 @@ describe('Task one: add and delete', () => {
   });
 });
 
+describe('task2', () => {
+  // Remove all completed
+  test('Remove all completed with the status of true', () => {
+    const mockTodo2 = {
+      description: 'remove completed',
+      id: 2,
+      completed: true,
+    };
+
+    const expected = [
+      {
+        description: 'json data',
+        id: 1,
+        completed: false,
+      },
+    ];
+
+    Storage.addTodo(mockTodo2);
+    Storage.deleteCompletedTask();
+
+    expect(Storage.getToDo()).toStrictEqual(expected);
+  });
+
+  // Update Status
+  test('Update status of item', () => {
+    const mockTodo2 = {
+      description: 'Update status',
+      id: 2,
+      completed: true,
+    };
+    const expected = [
+      {
+        description: 'json data',
+        id: 1,
+        completed: false,
+      },
+      {
+        description: 'Update status',
+        id: 2,
+        completed: true,
+      },
+    ];
+
+    Storage.addTodo(mockTodo2);
+    Storage.checkboxCompleted(2, true);
+    expect(Storage.getToDo()).toStrictEqual(expected);
+  });
+
   // edit description
   test('Edit updated description', () => {
     const tasks = {
